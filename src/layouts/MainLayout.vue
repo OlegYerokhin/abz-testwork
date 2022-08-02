ea85fd6b3707983f7bb88ff1d12187c13f6f0a03
OlegYerokhin <olegyerokhin91@gmail.com>
2022-08-02 20:58:24 +0300

    Init project

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-light-gray">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-img src="../assets/Logo.svg" alt="" />
+        </q-toolbar-title>
+
+        <div>btn-1</div>
+        <div>btn-2</div>
+      </q-toolbar>
+    </q-header>
+
+    
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'MainLayout',
+  components: {},
+  data () {
+    return {
+    }
+  }
+}
+</script>
